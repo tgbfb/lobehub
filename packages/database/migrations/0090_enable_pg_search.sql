f692b6483cb97bd4176156fd18af5ac229680b8f
Anton Fedyanin <krewger@yandex.ru>
2026-06-19 00:08:29 +0300

    chore: fix db

--- packages/database/migrations/0090_enable_pg_search.sql
+++ packages/database/migrations/0090_enable_pg_search.sql
@@ -1,2 +1,10 @@
 -- Custom SQL migration file, put your code below! --
-CREATE EXTENSION IF NOT EXISTS pg_search;
+-- pg_search (ParadeDB) is deprecated on Neon; skip gracefully if unavailable
+DO $$
+BEGIN
+  CREATE EXTENSION IF NOT EXISTS pg_search;
+EXCEPTION
+  WHEN OTHERS THEN
+    RAISE NOTICE 'pg_search extension not available (%), skipping', SQLERRM;
+END
+$$;
